20ce850fef8d98382f1489a8901001093e743efe
Diego Torres <diegoetorres1@gmail.com>
2026-07-20 10:16:21 -0400

    moving gsap anims to html file

--- js/main.js
+++
@@ -1,3 +0,0 @@
-const heroHeading = document.querySelector(".hero-heading");
-const heroText = document.querySelector(".hero-text");
-const projectContainer = document.querySelectorAll(".project-container");
